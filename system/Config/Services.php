6569ce1de6de082cd5d76b295d3bc6b5f7917103
MGatner <mgatner@icloud.com>
2021-06-01 01:04:18 +0000

    Implement Address and Email

--- system/Config/Services.php
+++ system/Config/Services.php
@@ -387,7 +387,7 @@ class Services extends BaseService
      *
      * @return \CodeIgniter\Mailer\MailerInterface
      */
-    public static function mailer($config = null, bool $getShared = true)
+    public static function mailer(MailerConfig $config = null, bool $getShared = true)
     {
         if ($getShared)
         {
@@ -396,7 +396,7 @@ class Services extends BaseService
 
         if (empty($config))
         {
-            $config = new \Config\MailerConfig();
+            $config = new MailerConfig();
         }
 
         $protocolMap = [
