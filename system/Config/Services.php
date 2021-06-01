d21f14989aa653b0b448b85ed5cce850ae426ff3
MGatner <mgatner@icloud.com>
2021-06-01 00:47:08 +0000

    Rebrand Mailer

--- system/Config/Services.php
+++ system/Config/Services.php
@@ -65,6 +65,7 @@ use Config\Filters as FiltersConfig;
 use Config\Format as FormatConfig;
 use Config\Honeypot as HoneypotConfig;
 use Config\Images;
+use Config\Mailer as MailerConfig;
 use Config\Migrations;
 use Config\Pager as PagerConfig;
 use Config\Services as AppServices;
@@ -376,6 +377,39 @@ class Services extends BaseService
         }
 
         return new Logger(config('Logger'));
+    }
+
+    /**
+     * The Mailer class lets you send composed messages via mail, sendmail, SMTP.
+     *
+     * @param MailerConfig|null $config
+     * @param boolean $getShared
+     *
+     * @return \CodeIgniter\Mailer\MailerInterface
+     */
+    public static function mailer($config = null, bool $getShared = true)
+    {
+        if ($getShared)
+        {
+            return static::getSharedInstance('mailer', $config);
+        }
+
+        if (empty($config))
+        {
+            $config = new \Config\MailerConfig();
+        }
+
+        $protocolMap = [
+            'mail'     => 'MailHandler',
+            'sendmail' => 'SendmailHandler',
+            'smtp'     => 'SMTPHandler',
+        ];
+
+        $handler   = '\\CodeIgniter\\Mailer\\Handlers\\' . ($protocolMap[$config->protocol ?? 'mail'] );
+        $mailer = new $handler($config);
+        $mailer->setLogger(static::logger(true));
+
+        return $mailer;
     }
 
     /**
