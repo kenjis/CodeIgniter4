44a77f6cfbf3ecaa29f65e036065da6f110a5a59
kenjis <kenji.uui@gmail.com>
2024-02-23 13:10:50 +0900

    fix: 'port' type

--- system/Session/Handlers/RedisHandler.php
+++ system/Session/Handlers/RedisHandler.php
@@ -97,6 +97,7 @@ class RedisHandler extends BaseHandler
 
             $this->savePath = [
                 'host'     => (! empty($matches[1]) ? $matches[1] : self::DEFAULT_PROTOCOL) . '://' . $matches[2],
+                'port'     => empty($matches[3]) ? self::DEFAULT_PORT : (int) $matches[3],
                 'password' => preg_match('#auth=([^\s&]+)#', $matches[4], $match) ? $match[1] : null,
                 'database' => preg_match('#database=(\d+)#', $matches[4], $match) ? (int) $match[1] : 0,
                 'timeout'  => preg_match('#timeout=(\d+\.\d+|\d+)#', $matches[4], $match) ? (float) $match[1] : 0.0,
